4010af8807c77c996fa89e053e6a2159dde46477
Victor Kendy Harada <victorkendy@gmail.com>
2011-01-06 22:44:29 -0200

    extracting vbo creation from test demo to separated file

--- vbo_rendering/main_tester.cpp
+++ vbo_rendering/main_tester.cpp
@@ -9,8 +9,9 @@
 #include "pbge/gfx/VBO.h"
 #include "pbge/gfx/StateSet.h"
 
-#include <GL/glew.h>
 #include <GL/glut.h>
+
+#include "vbo_setup.h"
 
 pbge::Node * root, * child, * trueRoot;
 pbge::TransformationNode * cam_node;
@@ -29,45 +30,12 @@ void display() {
     glutSwapBuffers();
 }
 
-void createNormalIndexes(std::vector<unsigned short> & ni) {
-    unsigned short indexes[] = {3,3,3,3, 5,5,5,5, 0,0,0,0, 2,2,2,2, 1,1,1,1, 4,4,4,4};
-    ni = std::vector<unsigned short>(indexes, indexes + 24);
-}
-
-void createVertexIndexes(std::vector<unsigned short> & vi) {
-    unsigned short indexes[] = {0,1,2,3, 7,4,1,0, 6,5,4,7, 3,2,5,6, 5,2,1,4, 0,3,6,7};
-    vi = std::vector<unsigned short>(indexes, indexes + 24);
-}
-
-void createVBOInstance() {
-    float v = 0.5f;
-    std::vector<unsigned short> vIndexes;
-    std::vector<unsigned short> nIndexes;
-    createNormalIndexes(nIndexes);
-    createVertexIndexes(vIndexes);
-    pbge::VertexBufferBuilder builder(24);
-    pbge::VertexAttribBuilder vertex = builder.addAttrib(3, pbge::VertexAttrib::VERTEX);
-    pbge::VertexAttribBuilder normal = builder.addAttrib(3, pbge::VertexAttrib::NORMAL);
-    pbge::VertexAttribBuilder color = builder.addAttrib(3, pbge::VertexAttrib::COLOR);
-
-    builder.pushValue(normal,1,0,0).pushValue(0,1,0).pushValue(0,0,1).pushValue(-1,0,0).pushValue(0,-1,0).pushValue(0,0,-1).setAttribIndex(nIndexes);
-    
-    builder.on(vertex).pushValue(-v,-v,-v).pushValue(-v,v,-v).pushValue(-v,v,v).pushValue(-v,-v,v)
-                      .pushValue(v,v,-v).pushValue(v,v,v).pushValue(v,-v,v).pushValue(v,-v,-v).setAttribIndex(vIndexes);
-
-    builder.pushValue(color, 0,0,0).pushValue(0,1,0).pushValue(0,1,1).pushValue(0,0,1);
-    builder.pushValue(1,1,0).pushValue(1,1,1).pushValue(1,0,1).pushValue(1,0,0).setAttribIndex(vIndexes);
-
-    pbge::VertexBuffer * vbo = builder.done();
-    vboModel = new pbge::ModelInstance(new pbge::VBOModel(vbo, GL_QUADS));
-}
-
 void setUp() {
     pbge::Manager::init();
     // FIXME: remove the state change line
     pbge::Manager::getInstance()->getOpenGL()->getState().enable(pbge::OpenGL::DEPTH_TEST);
     glClearColor(0,0,0,0);
-    createVBOInstance();
+    vboModel = createVBOInstance();
     renderer = new pbge::Renderer(pbge::Manager::getInstance()->getOpenGL());
     pbge::TransformationNode * node = new pbge::TransformationNode;
     math3d::matrix44 m = math3d::identity44;
---
+++ vbo_rendering/vbo_setup.h
@@ -0,0 +1,8 @@
+
+#ifndef VBO_SETUP_H_
+#define VBO_SETUP_H_
+
+#include "pbge/gfx/Model.h"
+pbge::ModelInstance * createVBOInstance();
+
+#endif
