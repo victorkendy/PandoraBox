57b1f33304fc3870c593306ef2504f596546f3ed
Victor Kendy Harada <victorkendy@gmail.com>
2011-02-11 19:26:54 -0200

    removing some commented code from vbo_rendering/main_tester.cpp

--- vbo_rendering/main_tester.cpp
+++ vbo_rendering/main_tester.cpp
@@ -6,17 +6,14 @@
 
 int cam_node_name;
 
-#ifndef M_PI
-#define M_PI 3.1415f
-#endif
-
 class CustomSceneInitializer : public pbge::SceneInitializer {
     pbge::SceneGraph * operator () (pbge::OpenGL * ogl) {
+        const float m_pi = 3.1415f;
         // FIXME: remove the state change line
         ogl->enableMode(pbge::OpenGL::DEPTH_TEST);
         pbge::ModelInstance * vboModel = createVBOInstance();
         pbge::SceneGraph * scene = new pbge::SceneGraph(new pbge::TransformationNode);
-        pbge::Node * child = scene->appendChildTo(pbge::SceneGraph::ROOT, pbge::TransformationNode::rotation(M_PI/3, 0,0,20)->scale(0.5f, 0.5f, 0.5f));
+        pbge::Node * child = scene->appendChildTo(pbge::SceneGraph::ROOT, pbge::TransformationNode::rotation(m_pi/3, 0,0,20)->scale(0.5f, 0.5f, 0.5f));
         pbge::Node * light_parent = scene->appendChildTo(pbge::SceneGraph::ROOT, pbge::TransformationNode::translation(0.0f, 1.0f, 0.0f));
         dynamic_cast<pbge::Light*>(scene->appendChildTo(light_parent, new pbge::PointLight))->setDiffuseColor(1,0,0,1);
         scene->appendChildTo(light_parent, new pbge::ModelInstance(new pbge::BezierCurve()));
@@ -30,25 +27,6 @@ class CustomSceneInitializer : public pbge::SceneInitializer {
         return scene;
     }
 };
-
-/*
-void setUp(pbge::OpenGL * ogl) {
-    
-    ogl->enableMode(pbge::OpenGL::DEPTH_TEST);
-    pbge::ModelInstance * vboModel = createVBOInstance();
-    scene = new pbge::SceneGraph(new pbge::TransformationNode);
-    pbge::Node * child = scene->appendChildTo(pbge::SceneGraph::ROOT, pbge::TransformationNode::rotation(M_PI/3, 0,0,20)->scale(0.5f, 0.5f, 0.5f));
-    pbge::Node * light_parent = scene->appendChildTo(pbge::SceneGraph::ROOT, pbge::TransformationNode::translation(0.0f, 1.0f, 0.0f));
-    dynamic_cast<pbge::Light*>(scene->appendChildTo(light_parent, new pbge::PointLight))->setDiffuseColor(1,0,0,1);
-    scene->appendChildTo(light_parent, new pbge::ModelInstance(new pbge::BezierCurve()));
-    cam_node_name = scene->appendChildTo(pbge::SceneGraph::ROOT, pbge::TransformationNode::translation(0.0f, 1.0f, 5.0f))->getSceneGraphIndex();
-    dynamic_cast<pbge::Light*>(scene->appendChildTo(cam_node_name, new pbge::PointLight))->setDiffuseColor(0,1,1,1);
-    scene->appendChildTo(child, vboModel);
-    pbge::CameraNode * cam = dynamic_cast<pbge::CameraNode*>(scene->appendChildTo(cam_node_name, new pbge::CameraNode()));
-    cam->lookAt(math3d::vector4(0,1,0), math3d::vector4(0,0,-1));
-    cam->setPerspective(45, 1, 1.0f, 10);
-    math3d::print_internal_memory_page_info();
-}*/
 
 /*
 void keyboard(unsigned char k, int x, int y) {
