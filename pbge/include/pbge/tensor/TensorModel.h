cba1183522079fea1e565b67a4149d07d1956ba2
Toshi Kurauchi <andrew.kurauchi@gmail.com>
2011-04-08 13:20:31 -0300

    Calculating eigenvectors and eigenvalues and adding tests

--- pbge/include/pbge/tensor/TensorModel.h
+++ pbge/include/pbge/tensor/TensorModel.h
@@ -5,22 +5,29 @@
 
 namespace pbge {
 
+    // Only symmetric 2D matrices (others are not implemented yet)
     class PBGE_EXPORT TensorModel : public Model {
     public:
-        TensorModel(float * tensor, int order);
+        TensorModel(float ** tensor, int order);
         ~TensorModel();
         float getEigenvalue(int index);
         float * getEigenvector(int index);
+
+        void render(ModelInstance * instance, OpenGL * ogl);
+        void renderDepth(ModelInstance * instance, OpenGL * ogl);
     private:
-        float * tensor;
+        float ** tensor;
         int order;
         float * eigenvalues;
         float ** eigenvectors;
-        boolean calculated;
+        bool calculated;
 
+        // Formulas based on Kingsley, Peter B. (2005). Introduction to Diffusion Tensor Imaging Mathematics: Part I. Tensors, Rotations, and Eigenvectors. Concepts in Magnetic Resonance Part A (Bridging Education and Research): 117-119
         void calculateEigenValuesAndVectors();
         void calculateEigenValuesAndVectors2d();
-        boolean tensorIsSymmetric();
+        void calculateEigenValues2dSymmetric();
+        void calculateEigenVectors2dSymmetric();
+        bool tensorIsSymmetric();
     };
 
 }
--- pbge/src/tensor/TensorModel.cpp
+++ pbge/src/tensor/TensorModel.cpp
@@ -1,9 +1,12 @@
+#include <stdlib.h>
+#include <math.h>
+
 #include "pbge/tensor/TensorModel.h"
 #include "pbge/exceptions/exceptions.h"
 
 using namespace pbge;
 
-TensorModel::TensorModel(float * _tensor, int _order) {
+TensorModel::TensorModel(float ** _tensor, int _order) {
     this->tensor = _tensor;
     this->order = _order;
     this->calculated = false;
@@ -43,7 +46,7 @@ void TensorModel::calculateEigenValuesAndVectors() {
     }
 }
 
-void TensorModel::tensorIsSymmetric() {
+bool TensorModel::tensorIsSymmetric() {
     for(int i = 0; i < this->order; i++) {
         for(int j = i + 1; j < this->order; j++) {
             if(this->tensor[i][j] != this->tensor[j][i]) {
@@ -55,5 +58,41 @@ void TensorModel::tensorIsSymmetric() {
 }
 
 void TensorModel::calculateEigenValuesAndVectors2d() {
-
+    if(this->tensorIsSymmetric()) {
+        this->calculateEigenValues2dSymmetric();
+        this->calculateEigenVectors2dSymmetric();
+    }
 }
+
+void TensorModel::calculateEigenValues2dSymmetric() {
+    float dxx = this->tensor[0][0];
+    float dyy = this->tensor[1][1];
+    float dxy = this->tensor[0][1];
+
+    this->eigenvalues = (float*)malloc(2*sizeof(float));
+    this->eigenvalues[0] = (dxx + dyy + sqrt((dxx+dyy)*(dxx+dyy) - 4*(dxx*dyy - dxy*dxy)))/2;
+    this->eigenvalues[1] = (dxx + dyy - sqrt((dxx+dyy)*(dxx+dyy) - 4*(dxx*dyy - dxy*dxy)))/2;
+}
+
+void TensorModel::calculateEigenVectors2dSymmetric() {
+    this->eigenvectors = (float**)malloc(2*sizeof(float*));
+    this->eigenvectors[0] = (float*)malloc(2*sizeof(float));
+    this->eigenvectors[1] = (float*)malloc(2*sizeof(float));
+
+    float dxx = this->tensor[0][0];
+    float dxy = this->tensor[0][1];
+    float eigenvalue1 = this->eigenvalues[0];
+    float vector_entry = (dxx - eigenvalue1)/dxy;
+    float magnitude = sqrt(1.0f + vector_entry*vector_entry);
+    float normalized_entry1 = 1.0f/magnitude;
+    float normalized_entry2 = vector_entry/magnitude;
+
+    this->eigenvectors[0][0] = normalized_entry1;
+    this->eigenvectors[0][1] = -normalized_entry2;
+    this->eigenvectors[1][0] = normalized_entry2;
+    this->eigenvectors[1][1] = normalized_entry1;
+}
+
+void TensorModel::render(ModelInstance * instance, OpenGL * ogl) {}
+
+void TensorModel::renderDepth(ModelInstance * instance, OpenGL * ogl) {}
